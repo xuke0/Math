e8a9377e277c707de0d0e316c48d4c1bfdd8054e
xuk08 <57583775+xuk08@users.noreply.github.com>
2020-11-18 13:04:20 -0500

    fix the function

--- BadMath/Source.cpp
+++ BadMath/Source.cpp
@@ -1,23 +1,26 @@
+// name: Ke Xu
+
+
 #include <iostream>
 #include <iomanip>
 using namespace std;
 
 float average(int i1, int i2, int i3)
 {
-	return i1 + i2 + i3 / 3.0f;
+    return (i1 + i2 + i3) / 3.0f;
 }
 
 int main()
 {
-	int n1 =0 , n2 = 0, n3 = 0;
-	cout << "This program calculates the average of three numbers." << endl;
-	cout << "First number: "; cin >> n1;
-	cout << "Second number: "; cin >> n2;
-	cout << "Third number: "; cin >> n2;
+    int n1 =0 , n2 = 0, n3 = 0;
+    cout << "This program calculates the average of three numbers." << endl;
+    cout << "First number: "; cin >> n1;
+    cout << "Second number: "; cin >> n2;
+    cout << "Third number: "; cin >> n3;
 
-	float a = average(n1, n2, n3);
+    float a = average(n1, n2, n3);
 
-	cout << setprecision(1) << fixed << "The average is " << a << endl;
+    cout << setprecision(2) << fixed << "The average is " << a << endl;
 
-	return 0;
+    return 0;
 }
